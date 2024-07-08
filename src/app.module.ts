c75cd450c45314ea4b71d766d36b1b185c3062e1
Wai Yan Myo <70007556+waiyanmyodev@users.noreply.github.com>
2024-07-08 23:38:49 +0630

    feat: [BZ-7] rate limiting with throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerBehindProxyGuard } from './common/src/guards/throttler-behind-proxy.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([{ 
+      ttl: 60000,
+      limit: 2,
+    }])
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerBehindProxyGuard
+    },
+  ],
 })
 export class AppModule {}
