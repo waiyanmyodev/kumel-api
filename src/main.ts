42d483f71973d3b7ae80ec6ac3d872065c924a81
Wai Yan Myo <70007556+waiyanmyodev@users.noreply.github.com>
2024-08-11 02:40:47 +0630

    feat: [BZ-41] setup swagger for api integration

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from "cookie-parser";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  const config = new DocumentBuilder()
+    .setTitle("API Documentation")
+    .setDescription("The API description")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(3000);
 }
 bootstrap();
