dd19aaa20a3c0f419eb1215456703317d37e3a3e
Wai Yan Myo <70007556+waiyanmyodev@users.noreply.github.com>
2025-04-04 22:27:16 +0630

    fix: remove admin guard in post module

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -14,16 +14,16 @@ import { CreatePostDto } from "./dto/create-post.dto";
 import { UpdatePostDto } from "./dto/update-post.dto";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { BasePaginationQueryDto } from "src/common/src/dto/base-pagination-query.dto";
-import { AdminJwtAuthGuard } from "src/common/src/guards/admin-jwt-auth.guard";
+import { UserJwtAuthGuard } from "src/common/src/guards/user-jwt-auth.guard";
 
 @ApiTags("Posts")
+@ApiBearerAuth()
+@UseGuards(UserJwtAuthGuard)
 @Controller("post")
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
-  @ApiBearerAuth()
-  @UseGuards(AdminJwtAuthGuard)
   create(@Body() createPostDto: CreatePostDto) {
     return this.postService.create(createPostDto);
   }
@@ -34,22 +34,16 @@ export class PostController {
   }
 
   @Get(":id")
-  @ApiBearerAuth()
-  @UseGuards(AdminJwtAuthGuard)
   findOne(@Param("id") id: string) {
     return this.postService.findOne(+id);
   }
 
   @Patch(":id")
-  @ApiBearerAuth()
-  @UseGuards(AdminJwtAuthGuard)
   update(@Param("id") id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postService.update(+id, updatePostDto);
   }
 
   @Delete(":id")
-  @ApiBearerAuth()
-  @UseGuards(AdminJwtAuthGuard)
   remove(@Param("id") id: string) {
     return this.postService.remove(+id);
   }
